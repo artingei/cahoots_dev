326f3cb95b2c5b219136c1161e63112d600986ad
artingei <artingei@gmail.com>
2014-06-12 18:27:11 +0200

    html structure & css: first changes

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -7,33 +7,21 @@ chrome.tabs.query({
     var tab = array_of_Tabs[0];
     // Example:
     var url = tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-    // document.getElementById('currentLink').innerHTML = url;
-    // document.getElementById('url').innerHTML = url;
+    document.getElementById('subtitle').innerHTML = url;
 
-    $.getJSON('../../test.json', function(jd) {
-    	console.log(jd)
-        document.getElementById('description_paper').innerHTML = jd.general_info.description;
-        document.getElementById('author_title_short').innerHTML = jd.people.jbittner.name;
-        document.getElementById('description_author').innerHTML = jd.people.jbittner.description;
-        document.getElementById('src_author').href = jd.people.jbittner.src;
-        document.getElementById('src_paper').href = jd.general_info.src;
+    $.getJSON("http://artingei.de/+++/mysql/"+url+".json", function(jd) {
+        //DEBUG
+        console.log(jd);
+        var num = jd.people.jbittner.cahoots;
+        console.log(num.length);
+        document.getElementById('title').innerHTML = "Ãœber " + jd.general_info.title;
+        document.getElementById('description').innerHTML = jd.general_info.description + '</a> <a class="quelle indesc" href="'+jd.general_info.src+'"> [QUELLE]</a>';
+        //document.getElementById('author_title').innerHTML = "FÃ¼r <span class='auth'>" + jd.people.jbittner.name + "</span> wurden die folgenden <span id='total_cahoots'>"+jd.people.jbittner.cahoots.length+"</span>Verbindungen gefunden:"; 
+        document.getElementById('author_title').innerHTML = "FÃ¼r <span class='auth'>" + jd.people.jbittner.name + "</span> wurden die folgenden Verbindungen gefunden:"; 
         $.each(jd.people.jbittner.cahoots, function(i, item) {
-            // $('#cahoots').append('<li class="cahoot_item" ><a href="'+ jd.people.jbittner.cahoots[i].url +'">'+ jd.people.jbittner.cahoots[i].name +'</a></li>');
-             $('#cahoot'+[i]).append(jd.people.jbittner.cahoots[i].name);
-            //$('.accordion').append('<dt>'+jd.people.jbittner.cahoots[i].name+'</dt><dd>'+jd.people.jbittner.cahoots[i].description+'</dd>');
+            //$('#cahoots').append('<li>' + jd.people.jbittner.cahoots[i].name + '</li>');
+            //$('#cahoots').append('<li class="cahoot_item" ><a href="'+ jd.people.jbittner.cahoots[i].url +'">'+ jd.people.jbittner.cahoots[i].name +'</a></li>');
+            $('#cahoots').append('<li class="cahoot_item" ><a class="links" title="Mehr Infos zu dieser Organisation" href="'+ jd.people.jbittner.cahoots[i].url +'">'+ jd.people.jbittner.cahoots[i].name +'</a> <a class="quelle" href="'+jd.people.jbittner.cahoots[i].src+'">[QUELLE]</a></li>');
         });
-        
-             // $('#stage').html('<p> Name: ' + jd.name + '</p>');
-             // $('#stage').append('<p>Age : ' + jd.age+ '</p>');
-             // $('#stage').append('<p> Sex: ' + jd.sex+ '</p>');
     });
-
-    // $.getJSON('http://artingei.de/cahoots/www.zeit.de.json', function( data ) {
-    // 	$('#test').html('<p> Name: ' + data.general_info.title + '</p>');
-    //  	console.log(data)
-    // )};
-    
-    //$("body").append('Test');
-
 });
-
