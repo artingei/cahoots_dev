15144929228836bdab6599bebf630e0eccd28d10
wuwi <forward.alba@gmail.com>
2014-06-17 11:46:23 +0200

    update

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,25 +8,23 @@ chrome.extension.sendMessage({}, function(response) {
 
 		var url = window.location.href;
 		var cleanUrl = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-		
-		console.log(cleanUrl); //DEBUG
+		console.log(cleanUrl);
 
+		// Get local stored .json containing site urls and IDs
 		$.getJSON(chrome.extension.getURL('test.json'), function(json) {
- 			//console.log(json);
- 			//console.log(cleanUrl);
- 			//console.log(json[cleanUrl].authors);
- 			
  			// Check if sites.json contains the clean URL of the current page
  			if (json.hasOwnProperty(cleanUrl)) {
+ 				// Iterate trough the .json file and see if any key for v.name is found in the .html document
  				$.each(json[cleanUrl].authors, function(i,v) {
  					if (document.documentElement.innerHTML.indexOf(v.name) > 0) {
- 						console.log(v.id);
+ 						console.log("found ID:"+v.id);
+ 						// Send message containing the ID to page_action
+ 						chrome.runtime.sendMessage({id: v.id}, function(response) {
+  							console.log(v.id);
+						});
  					}
- 				});
-				
+ 				});	
  			}
- 			
- 			
 		});
 
 		
