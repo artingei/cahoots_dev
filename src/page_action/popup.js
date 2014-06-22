169eecb19d90f54d278c71a4bc68c9c8a100c00c
artingei <artingei@gmail.com>
2014-06-22 18:03:34 +0200

    Revert 8c4cb03..3fb5946
    This rolls back to commit 8c4cb03616de1ce244d1eae2783c7e5a7478f0bd.

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -34,38 +34,29 @@ chrome.tabs.query({
 
 
 chrome.runtime.sendMessage({'method':'get_site'},function(response_site){
-<<<<<<< HEAD
-  var site = response_site;
-  document.getElementById('paper_name2').innerHTML = site;
-
-  chrome.runtime.sendMessage({'method':'get_siteinfo'},function(response_siteinfo){
-    var siteinfo = response_siteinfo;
-    document.getElementById('paper_description').innerHTML = siteinfo;
-    });
-=======
     var site = response_site;
     chrome.runtime.sendMessage({'method':'get_id'},function(response_id){
         var id = response_id;
         $.getJSON("../../db.json", function(db) {
             $.each(db.sites, function(i,v) {
                 if ( v.site_id == site ) {
-                    document.getElementById('paper_name2').innerHTML = v.name;
+                    document.getElementById('paper_name').innerHTML = v.name;
                     document.getElementById('paper_description').innerHTML = v.info;
                 }
             });
 
-            console.log(id);
 
-            var str_id = "ID"+id;
-
-            $.each(db.authors.ID0001.cahoots, function(i,v) {
-                $('#cahoots').append('<li>' + v.name + '</li>');
+            $.each(db.authors, function(i,v) {
+                if (v.id == id) {
+                    $('select').append('<option selected id="author_title_short">'+v.name+'</option>');
+                    $.each(v.cahoots, function(j,w){
+                        //$('#cahoots').append( '<li class="cahoot_item">'+w.name+'<a class="quelle" href="'+w.more_info+'">Info</a><a class="quelle" href="'+w.src+'">Source</a></li>' );
+                        $('#cahoots').append( '<li class="cahoot_item"><a title="Mehr Infos zu dieser Organisation" href="'+w.more_info+'">'+w.name+'</a><a class="quelle" href="'+w.src+'">Quelle</a></li>' );
+                    });
+                }
             });
         });
->>>>>>> parent of 2177711... update
 
-  chrome.runtime.sendMessage({'method':'get_id'},function(response_id){
-    var id = response_id;
     });
 });
 
