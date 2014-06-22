a3340d32295ad643c38b33007c531678df608d12
wuwi <forward.alba@gmail.com>
2014-06-22 18:30:23 +0200

    asd

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -1,38 +1,3 @@
-
-/*
-chrome.tabs.query({
-    active: true,               // Select active tabs
-    lastFocusedWindow: true     // In the current window
-}, function(array_of_Tabs) {
-    // Since there can only be one active tab in one active window, 
-    //  the array has only one element
-    var tab = array_of_Tabs[0];
-    // Example:
-    var url = tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-    //document.getElementById('paper_name').innerHTML = "Über " + url;
-
-    $.getJSON("../../sites.json", function(jd) {
-        //DEBUG
-        console.log(jd);
-        //var num = jd.people.jbittner.cahoots;
-        //console.log(num.length);
-        document.getElementById('paper_name2').innerHTML = jd.zeit.name;
-        document.getElementById('description').innerHTML = jd.general_info.description + '</a> <a class="quelle indesc" href="'+jd.general_info.src+'"> [QUELLE]</a>';
-        //document.getElementById('author_title').innerHTML = "FÃ¼r <span class='auth'>" + jd.people.jbittner.name + "</span> wurden die folgenden <span id='total_cahoots'>"+jd.people.jbittner.cahoots.length+"</span>Verbindungen gefunden:"; 
-        document.getElementById('author_title').innerHTML = "FÃ¼r <span class='auth'>" + jd.people.jbittner.name + "</span> wurden die folgenden Verbindungen gefunden:"; 
-        $.each(jd.people.jbittner.cahoots, function(i, item) {
-            //$('#cahoots').append('<li>' + jd.people.jbittner.cahoots[i].name + '</li>');
-            //$('#cahoots').append('<li class="cahoot_item" ><a href="'+ jd.people.jbittner.cahoots[i].url +'">'+ jd.people.jbittner.cahoots[i].name +'</a></li>');
-            $('#cahoots').append('<li class="cahoot_item" ><a class="links" title="Mehr Infos zu dieser Organisation" href="'+ jd.people.jbittner.cahoots[i].url +'">'+ jd.people.jbittner.cahoots[i].name +'</a> <a class="quelle" href="'+jd.people.jbittner.cahoots[i].src+'">[QUELLE]</a></li>');
-        });
-    });
-});
-
-*/
-
-
-
-
 chrome.runtime.sendMessage({'method':'get_site'},function(response_site){
     var site = response_site;
     chrome.runtime.sendMessage({'method':'get_id'},function(response_id){
@@ -40,7 +5,7 @@ chrome.runtime.sendMessage({'method':'get_site'},function(response_site){
         $.getJSON("../../db.json", function(db) {
             $.each(db.sites, function(i,v) {
                 if ( v.site_id == site ) {
-                    document.getElementById('paper_name').innerHTML = v.name;
+                    document.getElementById('paper_name2').innerHTML = v.name;
                     document.getElementById('paper_description').innerHTML = v.info;
                 }
             });
