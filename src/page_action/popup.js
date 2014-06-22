217771119c7be8372dc17d6de8bfb95f9f8e1fa0
wuwi <forward.alba@gmail.com>
2014-06-22 17:55:45 +0200

    update

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -40,17 +40,20 @@ chrome.runtime.sendMessage({'method':'get_site'},function(response_site){
         $.getJSON("../../db.json", function(db) {
             $.each(db.sites, function(i,v) {
                 if ( v.site_id == site ) {
-                    document.getElementById('paper_name2').innerHTML = v.name;
+                    document.getElementById('paper_name').innerHTML = v.name;
                     document.getElementById('paper_description').innerHTML = v.info;
                 }
             });
 
-            console.log(id);
 
-            var str_id = "ID"+id;
-
-            $.each(db.authors.ID0001.cahoots, function(i,v) {
-                $('#cahoots').append('<li>' + v.name + '</li>');
+            $.each(db.authors, function(i,v) {
+                if (v.id == id) {
+                    $('select').append('<option selected id="author_title_short">'+v.name+'</option>');
+                    $.each(v.cahoots, function(j,w){
+                        //$('#cahoots').append( '<li class="cahoot_item">'+w.name+'<a class="quelle" href="'+w.more_info+'">Info</a><a class="quelle" href="'+w.src+'">Source</a></li>' );
+                        $('#cahoots').append( '<li class="cahoot_item"><a title="Mehr Infos zu dieser Organisation" href="'+w.more_info+'">'+w.name+'</a><a class="quelle" href="'+w.src+'">Quelle</a></li>' );
+                    });
+                }
             });
         });
 
