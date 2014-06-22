e1141466c7a5078fed97d0204dca6c9fdc456d21
artingei <artingei@gmail.com>
2014-06-22 18:00:33 +0200

    Revert 2177711..8c4cb03
    This rolls back to commit 217771119c7be8372dc17d6de8bfb95f9f8e1fa0.

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -34,29 +34,16 @@ chrome.tabs.query({
 
 
 chrome.runtime.sendMessage({'method':'get_site'},function(response_site){
-    var site = response_site;
-    chrome.runtime.sendMessage({'method':'get_id'},function(response_id){
-        var id = response_id;
-        $.getJSON("../../db.json", function(db) {
-            $.each(db.sites, function(i,v) {
-                if ( v.site_id == site ) {
-                    document.getElementById('paper_name').innerHTML = v.name;
-                    document.getElementById('paper_description').innerHTML = v.info;
-                }
-            });
+  var site = response_site;
+  document.getElementById('paper_name2').innerHTML = site;
 
+  chrome.runtime.sendMessage({'method':'get_siteinfo'},function(response_siteinfo){
+    var siteinfo = response_siteinfo;
+    document.getElementById('paper_description').innerHTML = siteinfo;
+    });
 
-            $.each(db.authors, function(i,v) {
-                if (v.id == id) {
-                    $('select').append('<option selected id="author_title_short">'+v.name+'</option>');
-                    $.each(v.cahoots, function(j,w){
-                        //$('#cahoots').append( '<li class="cahoot_item">'+w.name+'<a class="quelle" href="'+w.more_info+'">Info</a><a class="quelle" href="'+w.src+'">Source</a></li>' );
-                        $('#cahoots').append( '<li class="cahoot_item"><a title="Mehr Infos zu dieser Organisation" href="'+w.more_info+'">'+w.name+'</a><a class="quelle" href="'+w.src+'">Quelle</a></li>' );
-                    });
-                }
-            });
-        });
-
+  chrome.runtime.sendMessage({'method':'get_id'},function(response_id){
+    var id = response_id;
     });
 });
 
